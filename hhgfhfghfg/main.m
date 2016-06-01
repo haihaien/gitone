8a1f3a90030df2480094e2bbeaa79ca94c4e19e2
LeMo-test <lemo-test@lemo-testdeimac.local>
2016-06-01 14:02:42 +0800

    Initial Commit

---
+++ hhgfhfghfg/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hhgfhfghfg
+//
+//  Created by LeMo-test on 16/6/1.
+//  Copyright © 2016年 LeMo-test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ hhgfhfghfgUITests/hhgfhfghfgUITests.m
@@ -0,0 +1,40 @@
+//
+//  hhgfhfghfgUITests.m
+//  hhgfhfghfgUITests
+//
+//  Created by LeMo-test on 16/6/1.
+//  Copyright © 2016年 LeMo-test. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hhgfhfghfgUITests : XCTestCase
+
+@end
+
+@implementation hhgfhfghfgUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
